6d743ffb7e32877065b39087c73718bcde54f842
Tatyana <43764894+taty2010@users.noreply.github.com>
2019-10-01 12:55:29 -0500

    Added useEffect and state properties

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -8,6 +8,36 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  
+   const [people, setPeople] = useState([]);
+  const [page, setPage] = useState("https://swapi.co/api/people/");
+  const [pageData, setPageData] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const people = await axios.get(`${page}`);
+        setPeople(people.data.results);
+        setPageData(people.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchData();
+  }, [page]);
+
+  function nextPage() {
+    if (page) {
+      return setPage(pageData.next);
+    }
+  }
+
+  function backPage() {
+    if (pageData.previous) {
+      return setPage(pageData.previous);
+    }
+  }
 
   return (
     <div className="App">
